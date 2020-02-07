915135d9ed1c57e6591359b2aaf0a394fdc5fa59
Malek.Zd <muhammadamin@pop-os.localdomain>
2020-02-07 03:51:19 +0200

    Transfer skeleton files into project folder I've moved the skeleton files into the project folder, then I started to modify my previous logic functions into the new logic file with the appropriate modification needed. Then I transfered the dom from the skeleton to the project folder and the index.html file then started modifying every thing in order to correct the mistake that had happend at the beginning of the project ... A little of css style modifying Relates #18

--- script/dom.js
+++ script/dom.js
@@ -1,3 +1,89 @@
+// part 2 linking it all together
+// The function here is called an iife,
+// it keeps everything inside hidden from the rest of our application
+(function() {
+    // This is the dom node where we will keep our todo
+    var container = document.getElementById('todo-container');
+    var addTodoForm = document.getElementById('add-todo');
+  
+    var state = [
+      { id: -3, description: 'first todo' },
+      { id: -2, description: 'second todo' },
+      { id: -1, description: 'third todo' },
+    ]; // this is our initial todoList
+  
+    // This function takes a todo, it returns the DOM node representing that todo
+    var createTodoNode = function(todo) {
+      var todoNode = document.createElement('li');
+      // you will need to use addEventListener
+  
+      // add span holding description
+      const todoSpan = document.createElement('span');
+      todoSpan.textContent = todo.description;
+  
+      // this adds the delete button
+      var deleteButtonNode = document.createElement('button');
+      deleteButtonNode.textContent = "Remove";
+      deleteButtonNode.addEventListener('click', function(event) {
+        var newState = todoFunctions.deleteTodo(state, todo.id);
+        update(newState);
+      });
+      
+  
+      // add markTodo button
+
+      const markTodoCheck = document.createElement('input');
+      markTodoCheck.setAttribute('type','checkbox');
+      markTodoCheck.className = "checkbox";
+  
+      // add classes for css
+  
+
+      todoNode.appendChild(todoSpan);
+      todoNode.appendChild(markTodoCheck);
+      todoNode.appendChild(deleteButtonNode);
+      return todoNode;
+    };
+  
+    // bind create todo form
+    if (addTodoForm) {
+      addTodoForm.addEventListener('submit', function(event) {
+        // https://developer.mozilla.org/en-US/docs/Web/Events/submit
+        // what does event.preventDefault do?
+        // what is inside event.target?
+        
+        var description = event.preventDefault(); // event.target ....
+
+        var newState = todoFunctions.addTodo(state,addTodoForm.description.value);
+        update(newState);
+      });
+    }
+  
+    // you should not need to change this function
+    var update = function(newState) {
+      state = newState;
+      renderState(state);
+    };
+  
+    // you do not need to change this function
+    var renderState = function(state) {
+      var todoListNode = document.createElement('ul');
+  
+      state.forEach(function(todo) {
+        todoListNode.appendChild(createTodoNode(todo));
+      });
+  
+      // you may want to add a class for css
+      container.replaceChild(todoListNode, container.firstChild);
+    };
+  
+    if (container) renderState(state);
+  })();
+
+
+/* ===============================================*/
+
+/*
 const inputName = document.querySelector('#addName');
 const inputDetails = document.querySelector('#addDetails');
 const inputDate = document.querySelector('#addDate');
@@ -57,3 +143,5 @@ for (let i = 0; i < dataArray.length; i++) {
     })
 
 }
+
+*/
